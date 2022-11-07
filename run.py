d7c523810635a3e29079e09c1e5c04c2efbb4321
2macs <adrian.mcneill@outlook.com>
2022-11-07 13:37:27 +0000

    print player and computer grids

--- run.py
+++ run.py
@@ -2,22 +2,38 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-ROWS = 10
-COLS = 10
+SIZE = 6
+NUMSHIPS = 4
+
+
 
 
 class GridBuilder:
     """
     Build the game grid
     """
-    def __init__(self, my_name, color, rows, cols):
+    def __init__(self, my_name, color, SIZE, NUMSHIPS):
         self.name = my_name
         self.color = color
-        self.rows = rows
-        self.cols = cols
-        print('Grid Builder class is called..')
-        print(f'Name is {self.name}, color is {self.color}, rows are {self.rows}, cols are {self.cols}.')
+        self.grid = SIZE    
+        self.numships = NUMSHIPS    
+        self.battleBoard = [['0' for x in range(SIZE)] for y in range(SIZE)]
+        
+
+    def printGrid(self):
+        """ Build and print the computer and player grids"""
+        print(f'{self.name} grid')
+        for row in self.battleBoard:
+            print("".join(row))
+        print('\n')
 
 
-GridBuilder('player', 'Blue', ROWS, COLS)
-GridBuilder('Computer', 'Green', ROWS, COLS)
+def RunGame():
+    print('Welcome to Battleship! Destroy the Enemy fleet!')
+    print('Empty sea is 0, ship loc is S, hit is *, miss is X' )
+    playerBoard = GridBuilder('player', 'Blue', SIZE, NUMSHIPS)
+    computerBoard = GridBuilder('Computer', 'Green', SIZE, NUMSHIPS)
+    GridBuilder.printGrid(playerBoard)
+    GridBuilder.printGrid(computerBoard)
+
+RunGame()
