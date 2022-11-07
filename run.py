99efae99ae9c0232c6cea2e6a0d5a7250ff2c573
2macs <adrian.mcneill@outlook.com>
2022-11-07 14:45:06 +0000

    generate 4 random locations for player ships

--- run.py
+++ run.py
@@ -2,10 +2,14 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+import random
+import os
+
 SIZE = 6
 NUMSHIPS = 4
+game_state_over = False 
 
-
+width = os.get_terminal_size().columns
 
 
 class GridBuilder:
@@ -18,6 +22,7 @@ class GridBuilder:
         self.grid = SIZE    
         self.numships = NUMSHIPS    
         self.battleBoard = [['0' for x in range(SIZE)] for y in range(SIZE)]
+        self.shipPositions = []       
         
 
     def printGrid(self):
@@ -26,14 +31,30 @@ class GridBuilder:
         for row in self.battleBoard:
             print("".join(row))
         print('\n')
+    
+    def PositionShips(self):
+        """ Randomly locate NUMSHIPS of ships on the board"""
+        i = 0 
+        while i < NUMSHIPS:
+            random_row = random.randint(0,SIZE - 1)
+            random_col = random.randint(0,SIZE - 1)
+            thisLoc = [random_row,random_col]
+            print(F'Rand row is {random_row}, random column is {random_col}')
+            self.shipPositions.append(thisLoc)
+            i += 1
+        print(self.shipPositions)
 
 
 def RunGame():
-    print('Welcome to Battleship! Destroy the Enemy fleet!')
-    print('Empty sea is 0, ship loc is S, hit is *, miss is X' )
     playerBoard = GridBuilder('player', 'Blue', SIZE, NUMSHIPS)
     computerBoard = GridBuilder('Computer', 'Green', SIZE, NUMSHIPS)
     GridBuilder.printGrid(playerBoard)
     GridBuilder.printGrid(computerBoard)
+    GridBuilder.PositionShips(playerBoard)
 
-RunGame()
+def main():
+    print('-----Welcome to Battleship! Destroy the Enemy fleet!-----'.center(width))
+    print('-----Empty sea is 0, ship loc is S, hit is *, miss is X-----\n'.center(width))
+    RunGame()
+
+main()
