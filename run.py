01572987b9ae83249a2e8125913bcb7e1b87f9b3
2macs <adrian.mcneill@outlook.com>
2022-11-07 12:37:18 +0000

    add GridBuilder class with dunder structure, confirm parameters are correctly passed

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+ROWS = 10
+COLS = 10
+
+
+class GridBuilder:
+    """
+    Build the game grid
+    """
+    def __init__(self, my_name, color, rows, cols):
+        self.name = my_name
+        self.color = color
+        self.rows = rows
+        self.cols = cols
+        print('Grid Builder class is called..')
+        print(f'Name is {self.name}, color is {self.color}, rows are {self.rows}, cols are {self.cols}.')
+
+
+GridBuilder('player', 'Blue', ROWS, COLS)
+GridBuilder('Computer', 'Green', ROWS, COLS)
